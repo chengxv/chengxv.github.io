9a2c8efa15c04792fe44bba7f76c26dea55b65dd
enjoyup <mac@cuicui.local>
2016-10-19 19:55:03 +0800

    继续修改中....

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,12 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes=[
-    {id:'ertong',name:'儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:'当当热销童书'},
-    {id:'lishi',name:'历史',img:'http://img3x0.ddimg.cn/32/5/24017630-1_l_9.jpg',description:'当当热销历史书籍'},
-    {id:'dongman',name:'动漫',img:'http://img3x8.ddimg.cn/20/27/24042368-1_l_6.jpg',description:'(当当热销动漫书籍'},
-    {id:'qingchunwenxue',name:'青春文学',img:'http://img3x4.ddimg.cn/34/5/24024364-1_l_12.jpg',description:'当当热销青春文学书籍'},
+    {id:'ertong',name:'儿童',img:'',description:'当当热销童书'},
+    {id:'lishi',name:'历史',img:'',description:'当当热销历史书籍'},
+    {id:'dongman',name:'动漫',img:'',description:'(当当热销动漫书籍'},
+    {id:'qingchunwenxue',name:'青春文学',img:'',description:'当当热销青春文学书籍'},
   ];
 }]);
+// http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg
+// http://img3x0.ddimg.cn/32/5/24017630-1_l_9.jpg
+// http://img3x8.ddimg.cn/20/27/24042368-1_l_6.jpg
+// http://img3x4.ddimg.cn/34/5/24024364-1_l_12.jpg
