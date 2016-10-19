bbe466c77685ee050d0247f74b7ccfbcf99c600c
enjoyup <mac@cuicui.local>
2016-10-19 17:06:41 +0800

    修改book.html详细信息

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes=[
-    {id:'ertong',name:'儿童',img:'',description:''},
-    {id:'lishi',name:'历史',img:'',description:''},
-    {id:'dongman',name:'动漫',img:'',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'',description:''},
+    {id:'ertong',name:'儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:'哈利·波特 系列第8个故事！特别彩排版剧本！中文版火爆预售！限时赠送霍格沃兹学校信封信笺套装！'},
+    {id:'lishi',name:'历史',img:'http://img3x0.ddimg.cn/32/5/24017630-1_l_9.jpg',description:'轰动世界的现象级畅销书，横扫英国、美国、德国、意大利、土耳其、印度等20多个国家。牛津大学知名历史学家全景展现两千年来始终主宰人类文明的世界十字路口，顺便了解 “一带一路”的前世今生！读客出品'},
+    {id:'dongman',name:'动漫',img:'http://img3x8.ddimg.cn/20/27/24042368-1_l_6.jpg',description:'(当当网随书赠送拼贴版存储罐2个，伟大的安妮主编，快看漫画重磅主题书，丁丁张、苑子文、苑子豪书写温暖推荐'},
+    {id:'qingchunwenxue',name:'青春文学',img:'http://img3x4.ddimg.cn/34/5/24024364-1_l_12.jpg',description:'当当签名本随机发；侧侧轻寒继《簪中录》后的时尚言情励志之作，这个故事将带你看到都市版《甄嬛传》，中国版《穿Prada的恶魔》以及时尚版《中国合伙人》。'},
   ];
 }]);
