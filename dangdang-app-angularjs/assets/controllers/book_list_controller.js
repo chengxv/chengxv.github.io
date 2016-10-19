052c9c10b63b128b6c86385c03e14bc7430145b6
enjoyup <mac@cuicui.local>
2016-10-19 21:14:14 +0800

    继续修改中...

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('bookListController',['$scope','commonService','$routeParams',function($scope,c_s,$routeParams){
-  // c_s.getData($routeParams.id,function(res){
-    console.dir($routeParams.id);
-  // });
-
-}]);
